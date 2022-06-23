bd16164dbc7b09be8c0a8cb0431136611d718bfd
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2022-06-23 01:14:42 -0700

    push

--- src/Components/MintBlack.js
+++ src/Components/MintBlack.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function MintBlack() {
   return (
-    <div to="/mint" className="col-md-6 text-white mint__left">
+    <div to="/mint" className="col-md-6 d-flex flex-column justify-content-between text-white mint__left">
       <div className="d-flex justify-content-between mint__left__top">
         {/* Top Left Small Text */}
         <h6 className="mt-2 col-4">THE HODLER IS STILL HOLDING</h6>
-        
+
         {/* Gray Box */}
         <div className="col-4 d-flex justify-content-center align-items-center">
           <img src="/images/grey.png" alt="" />
@@ -16,16 +16,19 @@ export default function MintBlack() {
         <h6 className="mt-2 col-4 text-end">UP ONLY</h6>
       </div>
 
-      <h3 className="mt-5">
-        WE BELIEVE IN OUR BAGS. <br /> WE NEVER SELL. LONG VISION. <br /> STOCK
-        TO FLOW IS OUR RELIGION. <br /> MICHAEL SAYLOR IS OUR GOD... <br /> BUT
-        THIS IS HARD TIME AND WE NEED SOME ETH, SO DONT BE GREEDY AND MINT SOME
-        HODLERS.
-      </h3>
-      <h1 className="my-5">
-        ROADMAP? <br /> JUST IN CASE WE’LL EARN ENOUGH ETH, WE ARE CURRENTLY
-        BROKE.
-      </h1>
+      {/* Big Texts */}
+      <div className="big__texts">
+        <h3 className="mt-5">
+          WE BELIEVE IN OUR BAGS. <br /> WE NEVER SELL. LONG VISION. <br />{" "}
+          STOCK TO FLOW IS OUR RELIGION. <br /> MICHAEL SAYLOR IS OUR GOD...{" "}
+          <br /> BUT THIS IS HARD TIME AND WE NEED SOME ETH, SO DONT BE GREEDY
+          AND MINT SOME HODLERS.
+        </h3>
+        <h1 className="my-5">
+          ROADMAP? <br /> JUST IN CASE WE’LL EARN ENOUGH ETH, WE ARE CURRENTLY
+          BROKE.
+        </h1>
+      </div>
 
       {/* Footer Small TExt */}
       <div className="footer py-1 d-flex align-items-center justify-content-between">
--- src/Components/MintWhite.js
+++ src/Components/MintWhite.js
@@ -3,11 +3,20 @@ import ConnectWallet from "./ConnectWallet";
 export default function MintWhite() {
   const [mint, setMint] = useState("noButtons");
   const [audioStatus, changeAudioStatus] = useState(false);
-  const showButtons = () => {
-    setMint("mintButtons");
-  };
 
+  // Play Audio
   const myRef = useRef();
+  const newref = useRef();
+
+  const startAudio = () => {
+    myRef.current.play();
+    //reduce speed of audio
+    //reduce voice of audio
+
+    myRef.current.playbackRate = 0.8;
+    myRef.current.volume = 0.5;
+    changeAudioStatus(true);
+  };
 
   //toogle audio on off
   const toggleAudio = () => {
@@ -20,13 +29,19 @@ export default function MintWhite() {
     }
   };
 
+  // Show Buttons
+  const showButtons = () => {
+    setMint("mintButtons");
+    startAudio();
+  };
+
   return (
     <div
       to="/mint"
       className="col-md-6 d-flex flex-column align-items-center justify-content-center bg-white mint__right"
     >
       {/* Audio */}
-      <audio ref={myRef} loop src="/images/m.mpeg" />
+      <audio ref={myRef} loop src="/images/music.mp3" />
 
       {/* Social Icons */}
       <div className="s__icons mt-1 flex align-items-center">
@@ -36,19 +51,17 @@ export default function MintWhite() {
         <a href="#" className="s_icon">
           <img src="/images/socials/opensea-icon.png" alt="" />
         </a>
-        <a
-          className="s_icon"
-          onClick={toggleAudio}
-        >
+        <a className="s_icon" onClick={toggleAudio}>
           {audioStatus ? (
-            <img src="/images/socials/play.png" />
-          ) : (
             <img src="/images/socials/pause.png" />
+          ) : (
+            <img src="/images/socials/play.png" />
           )}
         </a>
       </div>
       <h6 className="mt-2 col-4 text-end">UP ONLY</h6>
 
+      {/* Red Box */}
       <img onClick={showButtons} src="/images/red.png" alt="" />
       <div className="text__small mt-3 text-center">
         <h4 className="m-0" onClick={showButtons}>
@@ -58,10 +71,14 @@ export default function MintWhite() {
           1 FREE TO MINT <br /> THEN O.01 ETH <br /> MAX 10X WALLET
         </h6>
       </div>
+
+      {/* Input Buttons */}
       <div className={mint}>
         <input min={0} placeholder="0" type="number" className="mint__button" />
         <ConnectWallet />
       </div>
+
+      {/* White Box Small Text */}
       <footer className="d-flex w-100 align-items-center justify-content-between">
         <div className="col-6">
           <h6 className="mx-3">DEATH VISION LAB</h6>
